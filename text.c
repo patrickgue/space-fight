4587531da8b23c0cbb19dc246dee509c15064ce6
patrickgue <me@patrickguenthard.ch>
2020-01-29 20:14:36 +0100

    added functions for narrow font

--- text.c
+++ text.c
@@ -16,13 +16,26 @@ void print_char(int x, int y, char *text, byte color)
   }
 }
 
+void print_narrow(int x, int y, char *text, byte color)
+{
+  int i = 0, xo = 0;
+  while(text[i] != '\0') {
+    byte *ch = font_narrow[text[i] - 0x20];
+    render_sprite_one_c(x + xo, y,6,8, ch, color);
+    xo+=6;
+    i++;
+  }
+}
+
 
 void print_large(int x, int y, char *text, byte color)
 {
   int i = 0, xo = 0;
   while(text[i] != '\0') {
-    byte *ch = large_font[text[i] - 0x41];
-    render_sprite_one_c(x + xo, y,8,10, ch, color);
+    if(text[i] >= 0x41 && text[i] < 0x41 + 26) {
+      byte *ch = large_font[text[i] - 0x41];
+      render_sprite_one_c(x + xo, y,8,10, ch, color);
+    }
     xo+=8;
     i++;
   }
--- text.h
+++ text.h
@@ -4,4 +4,5 @@
 
 void print_char(int x, int y, char *, byte);
 void print_large(int x, int y, char * t, byte);
+void print_narrow(int x, int y, char * t, byte);
 #endif
