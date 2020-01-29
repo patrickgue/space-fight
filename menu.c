a1b008774662571fb2745735f1af1ea302212e19
patrickgue <me@patrickguenthard.ch>
2020-01-29 20:17:29 +0100

    added splashscreen and menu functions

--- menu.c
+++ menu.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "vga.h"
 #include "sprite.h"
 #include "sprites.h"
 #include "text.h"
+#include "game.h"
+#include "keyboard.h"
+#include "menu.h"
 
-void show_menu(int x, int y, int w, int h, char selections[11][32], int selections_size, int *selection, char *title) {
+void show_menu(int x, int y, int w, int h, char selections[11][32], int selections_size, int selection, char *title) {
   int index;
   
   rect_fill(x+4,y+4,w-8,h-8,0xf);
@@ -37,12 +41,90 @@ void show_menu(int x, int y, int w, int h, char selections[11][32], int selectio
   
   
   for(index = 0; index < selections_size; index++) {
-    if(*selection == index) {
+    if(selection == index) {
       rect_fill(x+4,y+19+(index * 10),w-8,10,104);
       print_char(x+5, y+20+(index * 10), selections[index], 0xf);
     }
     else {
       print_char(x+5, y+20+(index * 10), selections[index], 104);
     }
+  }
+}
+
+
+void splash_screen()
+{
+
+  splash_background();
+  rect_fill(19,38,50,1,36);
+
+  
+  print_large(21,30,"SPACE", 104);
+  
+  rect_fill(19,53,50,1,36);
+  print_large(21,45,"FIGHT", 104);
+
+  print_narrow(19,180,"Press Enter to continue", 104);
+  
+  render_sprite_rotate(120,60,96,40,splash,5);
+  show_buffer();
+
+  release_pressed(KEY_ENTER);
+
+  while(!is_pressed(KEY_ENTER))
+    update_pressed_keys();
+  while(is_pressed(KEY_ENTER))
+    update_pressed_keys();
+
+}
+
+
+void splash_background()
+{
+  int x, y;
+  
+  for(x = 0; x < SCREEN_WIDTH; x++) {
+    for(y = 0; y < SCREEN_HEIGHT; y++) {
+      int c = 76 + ((x * y * 17.0) / (320.0 * 200));
+      putpixel(x, y, c);
+    }
+  }
+}
+
+void show_help()
+{
+  bool show_help_loop = true;
+  int help_selection = 0;
+  int index;
+  int max_height = 17;
+
+  while(show_help_loop) {
+    clear_buffer(0x64);
+    rect_fill(19,18,320-40,1,36);
+    print_large(20,10,"HELP", 104);
+
+    rect_fill(19,175,320-40,1,36);
+    print_char(20,177,"Esc: back   Cursor Keys: scroll", 104);
+
+    
+    for(index = 0; index < max_height; index++) {
+      if(help_text[index + help_selection][0] == '#') {
+	print_char(20, 22 + (index * 9), help_text[index + help_selection] + 1, 104);
+      }
+      else 
+	print_narrow(20, 22 + (index * 9), help_text[index + help_selection], 104);
+    }
+
+    update_pressed_keys();
+    if(is_pressed_single(KEY_ESC))
+       show_help_loop = false;
+
+    if(help_selection > 0 && is_pressed_single(KEY_UP))
+      help_selection--;
+
+    if(help_selection < help_text_length - max_height && is_pressed_single(KEY_DOWN))
+      help_selection++;
+    
+    show_buffer();
   }
 }
--- menu.h
+++ menu.h
@@ -1,6 +1,43 @@
 #ifndef MENU_H
 #define MENU_H
 
-void show_menu(int x, int y, int w, int h, char selections[11][32], int size, int *selection, char*);
+#include "game.h"
+
+static char help_text[23][46] =
+  {
+   "#Description:",
+   "",
+   "Space Fight is a game where you ",
+   "have to fight a alien invasion",
+   "",
+   "#Keys in the menu:",
+   "",
+   "Cursor Keys:    select previous or next entry",
+   "Enter:          select menu entry",
+   "",
+   "#Keys in the game",
+   "",
+   "Cursor Keys:    control the spaceship",
+   "Spacebar:       fire bullet",
+   "Esc:            pause game",
+   "a",
+   "b",
+   "c",
+   "d",
+   "e",
+   "f",
+   "g",
+   "h",
+  };
+static int help_text_length = 23;//14;
+
+
+void show_menu(int, int, int, int, char selections[11][32], int size, int selection, char*);
+
+void splash_screen();
+
+void splash_background();
+
+void show_help();
 
 #endif
